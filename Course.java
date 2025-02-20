ca793fd0f037213e7f57fd433ffddc141d2d669b
Ploum <keo.ployer@ynov.com>
2025-02-20 11:16:48 +0100

    tout

---
+++ Course.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+public class Course{
+    private String courseName;
+    private String courseCode; 
+    private int creditHours;
+    private List<Student> students;
+
+    public Course(String courseName, String courseCode, int creditHours) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.creditHours = creditHours;
+        this.students = new ArrayList<>();
+    }   
+
+    public void enrollStudent(Student student){
+        students.add(student);
+    }
+
+
+    public List<Student> getEnrolledStudents() {
+        return students;
+    }
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+}
---
+++ Enrollment.java
@@ -0,0 +1,29 @@
+public class Enrollment {
+    private Student student;
+    private Course course;
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public void register() {
+        course.enrollStudent(student);
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
